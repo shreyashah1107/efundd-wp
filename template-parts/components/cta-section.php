131c44fe30bdbdd8d9d9db9f4cc38499c781a88f
Shreya Shah <shreya@darwindigital.com>
2024-11-18 16:01:27 +0530

    Updated Component UI

--- template-parts/components/cta-section.php
+++ template-parts/components/cta-section.php
@@ -2,7 +2,7 @@
 $cta_section = get_field("cta_section");
 if (isset($cta_section)): ?>
     <!-- Start: Checkout -->
-    <section class="checkout">
+    <section class="checkout checkout--components">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-xxl-10">
@@ -15,22 +15,22 @@ if (isset($cta_section)): ?>
                             <div class="btn-grp">
                                 <?php if (!empty($cta_section["button_text_1"])): ?>
                                     <a href="<?php echo @$cta_section["button_link_1"] ?>"
-                                       title="<?php echo $cta_section["button_text_1"] ?>"
-                                       class="btn btn-light-green btn-rd-15"><?php echo $cta_section["button_text_1"] ?></a>
+                                        title="<?php echo $cta_section["button_text_1"] ?>"
+                                        class="btn btn-light-green"><?php echo $cta_section["button_text_1"] ?></a>
                                 <?php endif; ?>
                                 <?php if (!empty($cta_section["button_text_2"])): ?>
                                     <a href="<?php echo $cta_section["button_link_2"] ?>"
-                                       title="<?php echo $cta_section["button_text_2"] ?>"
-                                       class="btn btn-light-green btn-rd-15"><?php echo $cta_section["button_text_2"] ?></a>
+                                        title="<?php echo $cta_section["button_text_2"] ?>"
+                                        class="btn btn-light-green"><?php echo $cta_section["button_text_2"] ?></a>
                                 <?php endif; ?>
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-xl-6 col-xxl-5 offset-xxl-1">
+                        <div class="col-lg-6 col-xl-6 col-xxl-6 offset-xxl-1 order-first order-xl-last">
                             <?php if (!empty($cta_section["image"])): ?>
-                                <div class="checkout--img">
+                                <div class="checkout--img text-xl-center">
                                     <img src="<?php echo $cta_section["image"]["url"] ?>"
-                                         alt="<?php echo @$cta_section["image"]["alt"] ?>">
+                                        alt="<?php echo @$cta_section["image"]["alt"] ?>">
                                 </div>
                             <?php endif; ?>
                         </div>
