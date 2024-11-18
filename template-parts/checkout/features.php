ae22be0fd4df29c68a98f9fea145f7639136c8f4
Shreya Shah <shreya@darwindigital.com>
2024-11-18 18:41:11 +0530

    Checkout UI

--- template-parts/checkout/features.php
+++ template-parts/checkout/features.php
@@ -1,23 +1,24 @@
 <?php
 $features = get_field("features");
 ?>
-<section class="security">
+<section class="security security--alt">
     <div class="container">
-        <div class="security-text--grid four-col-text--grid">
+        <div class="security-text--grid security-text--grid-four-col">
             <?php if (!empty($features)): foreach ($features as $feature): ?>
-                <div class="security-text--col">
-                    <?php if (!empty($feature["icon"])) : ?>
-                        <div class="security-icon">
-                            <img src="<?php echo $feature["icon"]["url"] ?>"
-                                 alt="<?php echo @$feature["icon"]["alt"] ?>">
+                    <div class="security-text--col">
+                        <?php if (!empty($feature["icon"])) : ?>
+                            <div class="security-icon">
+                                <img src="<?php echo $feature["icon"]["url"] ?>"
+                                    alt="<?php echo @$feature["icon"]["alt"] ?>">
+                            </div>
+                        <?php endif; ?>
+                        <div class="security-content">
+                            <h4><?php echo $feature["title"] ?></h4>
+                            <p><?php echo $feature["text"] ?></p>
                         </div>
-                    <?php endif; ?>
-                    <div class="security-content">
-                        <h4><?php echo $feature["title"] ?></h4>
-                        <p><?php echo $feature["text"] ?></p>
                     </div>
-                </div>
-            <?php endforeach; endif; ?>
+            <?php endforeach;
+            endif; ?>
         </div>
     </div>
 </section>
--- template-parts/checkout/white-content-section.php
+++ template-parts/checkout/white-content-section.php
@@ -1,32 +1,32 @@
 <?php
 $content_section = get_field("second_content_section");
 if (isset($content_section)):
-    ?>
-    <section class="worldwide-payments" style="background: #ffffff">
+?>
+    <section class="worldwide-payments bg-white">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-xxl-10">
                     <div class="row align-items-center justify-content-between">
-                        <div class="col-xl-6 col-xxl-5">
+                        <div class="col-xl-6 col-xxl-5 mb-5 mb-xl-0">
                             <div class="modular-solutions--img text-lg-center">
                                 <img src="<?php echo @$content_section["image"]["url"] ?>"
-                                     alt="<?php echo @$content_section["image"]["alt"] ?>">
+                                    alt="<?php echo @$content_section["image"]["alt"] ?>">
                             </div>
                         </div>
-                        <div class="col-xl-6 order-first order-xl-last">
+                        <div class="col-xl-6">
                             <div class="subtitle"><?php echo $content_section["highlighted_text"] ?></div>
                             <h3 class="section-title has-bg-gradient"><?php echo $content_section["title"] ?></h3>
                             <p><?php echo $content_section["text"] ?></p>
                             <div class="btn-grp">
                                 <?php if (!empty($content_section["button_title"])): ?>
                                     <a href="<?php echo @$content_section["button_link"] ?>"
-                                       title="<?php echo $content_section["button_title"] ?>"
-                                       class="btn btn-light-green btn-rd-15"><?php echo $content_section["button_title"] ?></a>
+                                        title="<?php echo $content_section["button_title"] ?>"
+                                        class="btn btn-light-green"><?php echo $content_section["button_title"] ?></a>
                                 <?php endif; ?>
                                 <?php if (!empty($content_section["secondary_button_title"])): ?>
                                     <a href="<?php echo $content_section["secondary_button_link"] ?>"
-                                       title="<?php echo $content_section["secondary_button_title"] ?>"
-                                       class="btn btn-light-green btn-rd-15"><?php echo $content_section["secondary_button_title"] ?></a>
+                                        title="<?php echo $content_section["secondary_button_title"] ?>"
+                                        class="btn btn-light-green"><?php echo $content_section["secondary_button_title"] ?></a>
                                 <?php endif; ?>
                             </div>
                         </div>
