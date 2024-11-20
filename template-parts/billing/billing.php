bd03ebf4cc1c77c4398b9c1f67acd7a2824bc34b
Shreya Shah <shreya@darwindigital.com>
2024-11-20 11:00:32 +0530

    Billing UI

---
+++ template-parts/billing/billing.php
@@ -0,0 +1,24 @@
+<section class="billing">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-xxl-11">
+        <div class="row align-items-center">
+          <div class="col-lg-7 col-xl-6 col-xxl-6">
+            <div class="title-row">
+              <div class="subtitle">BILLING</div>
+              <h3 class="section-title">Improve your accounts receivable process to be more efficient & automated!</h3>
+              <p>Get your invoices collected automatically & forget about missing payments. Get started!</p>
+            </div>
+            <div class="btn-grp">
+              <a href="#" title="Start Billing" class="btn btn-light-green">Start Billing</a>
+              <a href="#" title="Learn more" class="btn btn-light-green">Learn more</a>
+            </div>
+          </div>
+          <div class="col-md-8 col-lg-5 col-xl-6 col-xxl-5 offset-xxl-1 mb-5 mb-lg-0 order-first order-lg-last">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/latest/billing.png" alt="Billing">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
