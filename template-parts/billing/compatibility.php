026853677e9dbad131a91fd810a32f4b6014625a
Shreya Shah <shreya@darwindigital.com>
2024-11-20 14:04:42 +0530

    Billing UI

--- template-parts/billing/compatibility.php
+++ template-parts/billing/compatibility.php
@@ -1,26 +1,43 @@
-<section class="compatibility">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-xxl-11">
-        <div class="row align-items-center">
-          <div class="col-lg-7 col-xl-6 col-xxl-6">
-            <div class="title-row">
-              <div class="subtitle">COMPATIBLE WITH ALL SYSTEMS</div>
-              <h3 class="section-title has-bg-gradient">Integrate billing with your existing workflow</h3>
-              <p>Merge all your sales channels with a single billing system that connects your order management software, billing platform, CRM, accounting, and other systems</p>
+<?php
+$payment_link_section_dark = get_field('payment_link_section_dark');
+if ($payment_link_section_dark):
+?>
+  <section class="compatibility">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-11">
+          <div class="row align-items-center">
+            <div class="col-lg-7 col-xl-6 col-xxl-6">
+              <div class="title-row">
+                <div class="subtitle"><?php echo @$payment_link_section_dark["highlighted_text"] ?></div>
+                <h3 class="section-title has-bg-gradient"><?php echo @$payment_link_section_dark["title"] ?></h3>
+                <p><?php echo @$payment_link_section_dark["text"] ?></p>
+
+                <div class="btn-grp">
+                  <?php if ($payment_link_section_dark["button_text_1"]): ?>
+                    <a href="<?php echo @$payment_link_section_dark["button_link_1"] ?>"
+                      title="<?php echo $payment_link_section_dark["button_text_1"] ?>"
+                      class="btn btn-light-green"><?php echo $payment_link_section_dark["button_text_1"] ?></a>
+                  <?php endif; ?>
+                  <?php if ($payment_link_section_dark["button_text_2"]): ?>
+                    <a href="<?php echo @$payment_link_section_dark["button_link_2"] ?>"
+                      title="<?php echo $payment_link_section_dark["button_text_2"] ?>"
+                      class="btn btn-light-green"><?php echo $payment_link_section_dark["button_text_2"] ?></a>
+                  <?php endif; ?>
+                </div>
+              </div>
             </div>
-            <div class="btn-grp">
-              <a href="#" title="Get started" class="btn btn-light-green">Get started</a>
-              <a href="#" title="Learn more" class="btn btn-light-green">Learn more</a>
-            </div>
-          </div>
-          <div class="col-md-8 col-lg-5 col-xl-6 col-xxl-6 mb-5 mb-xl-0 pb-5 pb-lg-0 order-first order-lg-last">
-            <div>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/latest/compatibility.png" alt="Img">
+            <div class="col-md-8 col-lg-5 col-xl-6 col-xxl-6 mb-5 mb-xl-0 pb-5 pb-lg-0 order-first order-lg-last">
+              <div>
+                <?php if ($payment_link_section_dark["image"]): ?>
+                  <img src="<?php echo $payment_link_section_dark["image"]["url"] ?>"
+                    alt="<?php echo @$payment_link_section_dark["image"]["alt"] ?>">
+                <?php endif; ?>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+<?php endif; ?>
