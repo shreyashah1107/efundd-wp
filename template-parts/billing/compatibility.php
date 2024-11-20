4471628e506cb0655ca6740722e5e69e52f58665
Shreya Shah <shreya@darwindigital.com>
2024-11-20 11:00:32 +0530

    Billing UI

---
+++ template-parts/billing/compatibility.php
@@ -0,0 +1,26 @@
+<section class="compatibility">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-xxl-11">
+        <div class="row align-items-center">
+          <div class="col-lg-7 col-xl-6 col-xxl-6">
+            <div class="title-row">
+              <div class="subtitle">COMPATIBLE WITH ALL SYSTEMS</div>
+              <h3 class="section-title has-bg-gradient">Integrate billing with your existing workflow</h3>
+              <p>Merge all your sales channels with a single billing system that connects your order management software, billing platform, CRM, accounting, and other systems</p>
+            </div>
+            <div class="btn-grp">
+              <a href="#" title="Get started" class="btn btn-light-green">Get started</a>
+              <a href="#" title="Learn more" class="btn btn-light-green">Learn more</a>
+            </div>
+          </div>
+          <div class="col-md-8 col-lg-5 col-xl-6 col-xxl-6 mb-5 mb-xl-0 pb-5 pb-lg-0 order-first order-lg-last">
+            <div>
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/latest/compatibility.png" alt="Img">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
