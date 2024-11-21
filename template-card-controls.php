b127bd8f25107f76ff7676d5d017c5f03831b90a
Neeraj <neeraj@darwindigital.com>
2024-11-21 11:42:28 +0530

    card controls

--- template-card-controls.php
+++ template-card-controls.php
@@ -8,17 +8,17 @@ if (!defined('ABSPATH')) {
 }
 get_header(); ?>
 <!-- Start: Hero -->
-<?php include "template-parts/contracts/hero-section.php" ?>
+<?php include "template-parts/card-controls/hero-section.php" ?>
 <!-- End: Hero -->
 
 <!-- Start: Features -->
-<?php include "template-parts/contracts/features.php" ?>
+<?php include "template-parts/card-controls/features.php" ?>
 <!-- End: Features -->
 
-<?php include "template-parts/contracts/content-section-gray.php" ?>
+<?php include "template-parts/card-controls/content-section-gray.php" ?>
 
 <!-- Start: Compatibility -->
-<?php include "template-parts/contracts/compatibility.php" ?>
+<?php include "template-parts/card-controls/compatibility.php" ?>
 <!-- End: Compatibility -->
 
 <?php get_footer(); ?>
---
+++ template-parts/card-controls/compatibility.php
@@ -0,0 +1,43 @@
+<?php
+$checkout_section = get_field('checkout_section');
+if ($checkout_section):
+    ?>
+    <section class="worldwide-payments bg-white">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xxl-11">
+                    <div class="row align-items-center">
+                        <div class="col-md-8 col-lg-5 col-xl-6 col-xxl-5 offset-xxl-1 mb-5 mb-xl-0 pb-5 pb-lg-0 order-first order-lg-last">
+                            <div>
+                                <?php if ($checkout_section["image"]): ?>
+                                    <img src="<?php echo $checkout_section["image"]["url"] ?>"
+                                         alt="<?php echo @$checkout_section["image"]["alt"] ?>">
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-xl-6 col-xxl-6">
+                            <div class="title-row">
+                                <div class="subtitle"><?php echo @$checkout_section["highlighted_text"] ?></div>
+                                <h3 class="section-title has-bg-gradient"><?php echo @$checkout_section["title"] ?></h3>
+                                <p><?php echo @$checkout_section["text"] ?></p>
+
+                                <div class="btn-grp">
+                                    <?php if ($checkout_section["button_title"]): ?>
+                                        <a href="<?php echo @$checkout_section["button_link"] ?>"
+                                           title="<?php echo $checkout_section["button_title"] ?>"
+                                           class="btn btn-light-green"><?php echo $checkout_section["button_title"] ?></a>
+                                    <?php endif; ?>
+                                    <?php if ($checkout_section["secondary_button_title"]): ?>
+                                        <a href="<?php echo @$checkout_section["secondary_button_link"] ?>"
+                                           title="<?php echo $checkout_section["secondary_button_title"] ?>"
+                                           class="btn btn-light-green"><?php echo $checkout_section["secondary_button_title"] ?></a>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
