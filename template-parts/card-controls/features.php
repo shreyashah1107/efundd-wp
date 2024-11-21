09788a093d690c4398aa6fe872b61d5e7d54e736
shreyashah1107 <shreyashah.117@gmail.com>
2024-11-21 12:10:43 +0530

    Card Controls UI updates

--- template-parts/card-controls/features.php
+++ template-parts/card-controls/features.php
@@ -3,12 +3,12 @@ $features = get_field('features');
 if (isset($features)):
     ?>
     <!-- Start: Features -->
-    <section class="features">
+    <section class="security security--alt">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-xxl-10">
                     <div class="row align-items-center">
-                        <div class="security-text--grid" style="grid-template-columns: repeat(3, minmax(0, 1fr));">
+                        <div class="security-text--grid">
                             <?php foreach ($features as $feature): ?>
                                 <div class="security-text--col">
                                     <?php if (!empty($feature["icon"])): ?>
