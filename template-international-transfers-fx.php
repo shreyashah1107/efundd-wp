19170eece712d8959967862c2239cccff393f3ee
Neeraj <neeraj@darwindigital.com>
2024-11-28 09:52:47 +0530

    cards pages

---
+++ template-international-transfers-fx.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Template Name: International Transfers FX Template
+ */
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+get_header(); ?>
+    <!-- Start: Hero -->
+    <?php include "template-parts/international-transfers-fx/hero-section.php" ?>
+    <!-- End: Hero -->
+
+    <!-- Start: Features -->
+    <?php include "template-parts/international-transfers-fx/features.php" ?>
+    <!-- End: Features -->
+
+    <!-- Start: Content Section  -->
+    <?php include "template-parts/international-transfers-fx/content-section-first.php" ?>
+    <!-- End: Content Section -->
+
+    <!-- Start: Content Section -->
+    <?php include "template-parts/international-transfers-fx/content-section-second.php" ?>
+    <!-- End: Content Section -->
+
+    <!-- Start: Content Section -->
+    <?php include "template-parts/international-transfers-fx/content-section-third.php" ?>
+    <!-- End: Content Section -->
+
+    <!-- Start: Content Section -->
+    <?php include "template-parts/international-transfers-fx/content-section-fourth.php" ?>
+    <!-- End: Content Section -->
+<?php get_footer(); ?>
