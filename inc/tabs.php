b1ef82de3784fd35078ca65763f390452d88565c
Neeraj <neeraj@darwindigital.com>
2024-12-27 10:00:49 +0530

    header changes

---
+++ inc/tabs.php
@@ -0,0 +1,36 @@
+<div class="tabs">
+    <?php
+    $menu_type = display_matching_menu();
+    global $post;
+    $current_slug = $post->post_name;
+    ?>
+    <?php if (is_front_page() || $menu_type == "corporate_menu") {
+        $menu_type ="corporate_menu";
+        ?>
+        <input type="radio" id="corporate-tab" name="tabs" checked/>
+        <label class="tab" for="corporate-tab">Corporate</label>
+    <?php } else {
+        ?>
+        <input type="radio" id="corporate-tab" name="tabs"/>
+        <label class="tab" for="corporate-tab">Corporate</label>
+    <?php } ?>
+    <?php if ($current_slug == 'business-landing-page' || $menu_type == "main") {
+        $menu_type ="main";
+        ?>
+        <input type="radio" id="business-tab" name="tabs" checked/>
+        <label class="tab" for="business-tab">Business</label>
+    <?php } else { ?>
+        <input type="radio" id="business-tab" name="tabs"/>
+        <label class="tab" for="business-tab">Business</label>
+    <?php } ?>
+    <?php if ($current_slug == 'personal-landing-page' || $menu_type == "personal_menu") {
+        $menu_type ="personal_menu";
+        ?>
+        <input type="radio" id="personal-tab" name="tabs" checked/>
+        <label class="tab" for="personal-tab">Personal</label>
+    <?php } else { ?>
+        <input type="radio" id="personal-tab" name="tabs"/>
+        <label class="tab" for="personal-tab">Personal</label>
+    <?php } ?>
+    <span class="glider"></span>
+</div>
