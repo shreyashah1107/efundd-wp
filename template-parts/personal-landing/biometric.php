0e90a387d9eede65cce20fb0d69b42ccc958beff
shreyashah1107 <shreyashah.117@gmail.com>
2024-12-29 10:25:37 +0530

    Updated Personal landing UI

---
+++ template-parts/personal-landing/biometric.php
@@ -0,0 +1,24 @@
+<section class="fingerpay-solution fingerpay-solution--biometric">
+  <div class="container">
+    <div class="row align-items-center justify-content-between justify-content-xxl-start">
+      <div class="col-lg-6 col-xl-5">
+        <div class="fingerpay-solution--img">
+          <img src="https://documented.in/funddpay/wp-content/uploads/2024/08/fingerpay.png" alt="">
+        </div>
+      </div>
+      <div class="col-lg-6 col-xl-6 col-xxl-6 offset-xxl-1">
+        <div class="title-row">
+          <div class="subtitle">BIOMETRIC</div>
+          <h3 class="section-title section-title--xl has-bg-gradient">Your money, always safeguarded</h3>
+          <p>Our biometric payment solution allows Paayco users to pay using their biometric fingerptint anywhere Paayco card machines are available. No more worrying about lost or stolen cards, card skimming, remembering your PIN or contactless payment limit.</p>
+
+          <p class="tagline">Powerful. Proven. Biometric Payments.</p>
+        </div>
+
+        <div class="btn-grp mt-5">
+          <a href="#" title="Learn more" class="btn btn-outline-light-green text-dark">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ template-parts/personal-landing/trusted-account.php
@@ -0,0 +1,55 @@
+<section class="crm pb-5">
+    <div class="container">
+        <div class="crm-content--wrapper">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-xl-6">
+                    <div class="title-row">
+                        <div class="subtitle">PERSONAL ACCOUNT</div>
+                        <h3 class="section-title section-title--lg text-uppercase">THE TRUSTED PERSONAL ACCOUNT</h3>
+                        <p>Receive an instant bank account number and sort code upon completing account registration.</p>
+                    </div>
+
+                    <div class="btn-grp mt-5">
+                        <a href="#" title="Open an account" class="btn btn-light-green">Open an account</a>
+                    </div>
+                </div>
+                <div class="col-xl-5">
+                    <div class="crm-cta--list">
+                        <div class="crm-cta--list-item">
+                            <div class="account-creation">
+                                <div class="account-creation--title">
+                                    <div class="icon">
+                                        <img src="https://documented.in/funddpay/wp-content/themes/efundd-wp-theme/assets/img/other/user.svg" alt="User Icon">
+                                    </div>
+                                    <div class="title">Create Account</div>
+                                </div>
+                                <div class="account-creation--badge">
+                                    <span class="text">Free</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="crm-cta--list-item">
+                            <div class="payment-info">
+                                <div class="payment-info--title">
+                                    <div class="flag">
+                                        <img src="https://documented.in/funddpay/wp-content/uploads/2024/08/uk.svg" alt="">
+                                    </div>
+                                    <div class="currency">GBP</div>
+                                </div>
+                                <div class="payment-info--account">
+                                    <div class="amount">£528.30</div>
+                                    <div class="account-number">
+                                        <span class="icon">
+                                            <img src="https://documented.in/funddpay/wp-content/uploads/2024/08/account.svg" alt="">
+                                        </span>
+                                        <span>... 4940</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
