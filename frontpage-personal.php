224a7971aef4eb9b6723dd32bb10c8d3276e9d75
Shreya Shah <shreya@darwindigital.com>
2025-01-18 14:50:19 +0530

    Updated hero section on personal landing UI & added animation on the same

--- frontpage-personal.php
+++ frontpage-personal.php
@@ -12,63 +12,51 @@ get_header(); ?>
 <!-- End: Hero -->
 
 <!-- Start: Payments -->
-<?php //include "template-parts/personal-landing/banking-slider.php" 
-?>
+<?php include "template-parts/personal-landing/banking-slider.php" ?>
 <!-- End: Payments -->
 
 <!-- Start: Multi Currency -->
-<?php //include "template-parts/personal-landing/multi-currency-cards.php" 
-?>
+<?php include "template-parts/personal-landing/multi-currency-cards.php" ?>
 <!-- End: Multi Currency -->
 
 <!-- Start: Monitor Transactions -->
-<?php //include "template-parts/personal-landing/account-management.php" 
-?>
+<?php include "template-parts/personal-landing/account-management.php" ?>
 <!-- End: Monitor Transactions -->
 
 <!-- Start: Payment Methods -->
-<?php //include "template-parts/personal-landing/corporate-cards.php" 
-?>
+<?php include "template-parts/personal-landing/corporate-cards.php" ?>
 <!-- End: Payment Methods -->
 
 <!-- Start: Payments -->
-<?php //include "template-parts/personal-landing/services-section.php" 
-?>
+<?php include "template-parts/personal-landing/services-section.php" ?>
 <!-- End: Payments -->
 
 <!-- Start: Money in Account -->
-<?php //include "template-parts/personal-landing/solution-section.php" 
-?>
+<?php include "template-parts/personal-landing/solution-section.php" ?>
 <!-- End: Money in Account -->
 
 <!-- Start: Mobile App -->
-<?php //include "template-parts/personal-landing/mobile-app-section.php" 
-?>
+<?php include "template-parts/personal-landing/mobile-app-section.php" ?>
 <!-- End: Mobile App -->
 
 <!-- Start: Trusted Account -->
-<?php //include "template-parts/personal-landing/trusted-account.php" 
-?>
+<?php include "template-parts/personal-landing/trusted-account.php" ?>
 <!-- Start: Trusted Account -->
 
 <!-- Start: Pricing -->
-<?php //include "template-parts/personal-landing/pricing-plans.php" 
-?>
+<?php include "template-parts/personal-landing/pricing-plans.php" ?>
 <!-- End: Pricing -->
 
 <!-- Start: Biometric -->
-<?php //include "template-parts/personal-landing/biometric.php" 
-?>
+<?php include "template-parts/personal-landing/biometric.php" ?>
 <!-- Start: Biometric -->
 
 <!-- Start: Security -->
-<?php //include "template-parts/home/security.php" 
-?>
+<?php include "template-parts/home/security.php" ?>
 <!-- End: Security -->
 
 <!-- Start: CTA -->
-<?php //include "template-parts/home/cta-section.php" 
-?>
+<?php include "template-parts/home/cta-section.php" ?>
 <!-- End: CTA -->
 
 <?php get_footer(); ?>
