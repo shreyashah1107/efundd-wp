b25245a4491a932b6397c0b7bed308816cc14651
Shreya Shah <shreya@darwindigital.com>
2025-01-18 14:46:34 +0530

    Updated hero section on personal landing UI & added animation on the same

--- frontpage-personal.php
+++ frontpage-personal.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Personal Landing Template
  */
@@ -6,56 +7,68 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 get_header(); ?>
-    <!-- Start: Hero -->
+<!-- Start: Hero -->
 <?php include "template-parts/personal-landing/hero-section.php" ?>
-    <!-- End: Hero -->
+<!-- End: Hero -->
 
-    <!-- Start: Payments -->
-<?php include "template-parts/personal-landing/banking-slider.php" ?>
-    <!-- End: Payments -->
+<!-- Start: Payments -->
+<?php //include "template-parts/personal-landing/banking-slider.php" 
+?>
+<!-- End: Payments -->
 
-    <!-- Start: Multi Currency -->
-    <?php include "template-parts/personal-landing/multi-currency-cards.php" ?>
-    <!-- End: Multi Currency -->
+<!-- Start: Multi Currency -->
+<?php //include "template-parts/personal-landing/multi-currency-cards.php" 
+?>
+<!-- End: Multi Currency -->
 
-    <!-- Start: Monitor Transactions -->
-<?php include "template-parts/personal-landing/account-management.php" ?>
-    <!-- End: Monitor Transactions -->
+<!-- Start: Monitor Transactions -->
+<?php //include "template-parts/personal-landing/account-management.php" 
+?>
+<!-- End: Monitor Transactions -->
 
-    <!-- Start: Payment Methods -->
-<?php include "template-parts/personal-landing/corporate-cards.php" ?>
-    <!-- End: Payment Methods -->
+<!-- Start: Payment Methods -->
+<?php //include "template-parts/personal-landing/corporate-cards.php" 
+?>
+<!-- End: Payment Methods -->
 
-    <!-- Start: Payments -->
-<?php include "template-parts/personal-landing/services-section.php" ?>
-    <!-- End: Payments -->
+<!-- Start: Payments -->
+<?php //include "template-parts/personal-landing/services-section.php" 
+?>
+<!-- End: Payments -->
 
-    <!-- Start: Money in Account -->
-<?php include "template-parts/personal-landing/solution-section.php" ?>
-    <!-- End: Money in Account -->
+<!-- Start: Money in Account -->
+<?php //include "template-parts/personal-landing/solution-section.php" 
+?>
+<!-- End: Money in Account -->
 
-    <!-- Start: Mobile App -->
-<?php include "template-parts/personal-landing/mobile-app-section.php" ?>
-    <!-- End: Mobile App -->
+<!-- Start: Mobile App -->
+<?php //include "template-parts/personal-landing/mobile-app-section.php" 
+?>
+<!-- End: Mobile App -->
 
-    <!-- Start: Trusted Account -->
-    <?php include "template-parts/personal-landing/trusted-account.php" ?>
-    <!-- Start: Trusted Account -->
+<!-- Start: Trusted Account -->
+<?php //include "template-parts/personal-landing/trusted-account.php" 
+?>
+<!-- Start: Trusted Account -->
 
-    <!-- Start: Pricing -->
-<?php include "template-parts/personal-landing/pricing-plans.php" ?>
-    <!-- End: Pricing -->
+<!-- Start: Pricing -->
+<?php //include "template-parts/personal-landing/pricing-plans.php" 
+?>
+<!-- End: Pricing -->
 
-    <!-- Start: Biometric -->
-    <?php include "template-parts/personal-landing/biometric.php" ?>
-    <!-- Start: Biometric -->
+<!-- Start: Biometric -->
+<?php //include "template-parts/personal-landing/biometric.php" 
+?>
+<!-- Start: Biometric -->
 
-    <!-- Start: Security -->
-<?php include "template-parts/home/security.php" ?>
-    <!-- End: Security -->
+<!-- Start: Security -->
+<?php //include "template-parts/home/security.php" 
+?>
+<!-- End: Security -->
 
-    <!-- Start: CTA -->
-<?php include "template-parts/home/cta-section.php" ?>
-    <!-- End: CTA -->
+<!-- Start: CTA -->
+<?php //include "template-parts/home/cta-section.php" 
+?>
+<!-- End: CTA -->
 
 <?php get_footer(); ?>
--- template-parts/personal-landing/hero-section.php
+++ template-parts/personal-landing/hero-section.php
@@ -2,8 +2,8 @@
 $hero_section = get_field("hero_section");
 if (isset($hero_section)):
     $hero_image = $hero_section["image"];
-    ?>
-    <section class="hero hero--personal">
+?>
+    <section class="hero hero--personal-updated">
         <div class="container">
             <!-- Active Page Text: Show < 576px -->
             <div class="d-sm-none text-center">
@@ -12,30 +12,54 @@ if (isset($hero_section)):
             <!-- Active Page Text -->
 
             <div class="hero--content">
-                <div class="row align-items-center justify-content-center justify-content-xl-between">
-                    <div class="col-xl-6 col-xxl-5 text-center text-xl-start">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-xl-10 text-center">
                         <h1><?php echo $hero_section["title"] ?></h1>
                         <p><?php echo $hero_section["subtitle"] ?></p>
 
-                        <div class="btn-grp justify-content-center justify-content-xl-start">
+                        <div class="btn-grp justify-content-center">
                             <?php if (!empty($hero_section["button_text_1"])): ?>
                                 <a href="<?php echo @$hero_section["button_link_1"] ?>"
-                                   title="<?php echo $hero_section["button_text_1"] ?>"
-                                   class="btn btn-light-green"><?php echo $hero_section["button_text_1"] ?></a>
+                                    title="<?php echo $hero_section["button_text_1"] ?>"
+                                    class="btn btn-light-green"><?php echo $hero_section["button_text_1"] ?></a>
                             <?php endif; ?>
                             <?php if (!empty($hero_section["button_text_2"])): ?>
                                 <a href="<?php echo $hero_section["button_link_2"] ?>"
-                                   title="<?php echo $hero_section["button_text_2"] ?>"
-                                   class="btn btn-outline-light-green"><?php echo $hero_section["button_text_2"] ?></a>
+                                    title="<?php echo $hero_section["button_text_2"] ?>"
+                                    class="btn btn-outline-gray"><?php echo $hero_section["button_text_2"] ?></a>
                             <?php endif; ?>
                         </div>
-                    </div>
 
-                    <div class="col-md-8 col-lg-8 col-xl-6 col-xxl-6">
-                        <div class="hero--img text-center mt-5 mt-xl-0 pt-5 pt-xl-0">
-                            <?php if (!empty($hero_image["url"])): ?>
-                                <img src="<?php echo $hero_image["url"] ?>" alt="<?php echo @$hero_image["alt"] ?>">
-                            <?php endif; ?>
+                        <div class="hero--content-images">
+                            <div class="images">
+                                <div class="images--center">
+                                    <div class="center-left">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/animated/1.png" alt="Image">
+                                    </div>
+                                    <div class="center-right">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/animated/2.png" alt="Image">
+                                    </div>
+                                </div>
+
+                                <div class="images--left">
+                                    <div class="left-top">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/animated/left-top.png" alt="Image">
+                                    </div>
+
+                                    <div class="left-bottom">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/animated/left-bottom.png" alt="Image">
+                                    </div>
+                                </div>
+
+                                <div class="images--right">
+                                    <div class="right-top">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/animated/right-top.png" alt="Image">
+                                    </div>
+                                    <div class="right-bottom">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/animated/right-bottom.png" alt="Image">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
