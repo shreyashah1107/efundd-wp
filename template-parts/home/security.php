056aec6a239edc1e1b577d15e30c408286f0ce79
Shreya Shah <shreya@darwindigital.com>
2025-01-20 18:23:04 +0530

    Updated 2 sections on personal landing page as per new figma design

--- template-parts/home/security.php
+++ template-parts/home/security.php
@@ -2,30 +2,34 @@
 $security_section = get_field("security_section");
 if (isset($security_section)):
     $securities = $security_section["securities"];
-    ?>
+?>
     <section class="security">
         <div class="container">
-            <div class="title-row text-center">
-                <div class="subtitle"><?php echo $security_section["highlighted_text"] ?></div>
-                <h3 class="section-title has-bg-gradient"><?php echo $security_section["title"] ?></h3>
-                <p><?php echo $security_section["text"] ?></p>
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-xxl-5">
+                    <div class="title-row text-center">
+                        <h3 class="section-title"><?php echo $security_section["title"] ?></h3>
+                        <p><?php echo $security_section["text"] ?></p>
+                    </div>
+                </div>
             </div>
 
             <div class="security-text--grid">
                 <?php if (!empty($securities)): foreach ($securities as $security): ?>
-                    <div class="security-text--col">
-                        <?php if (!empty($security["icon"])) : ?>
-                            <div class="security-icon">
-                                <img src="<?php echo $security["icon"]["url"] ?>"
-                                     alt="<?php echo @$security["icon"]["alt"] ?>">
+                        <div class="security-text--col">
+                            <?php if (!empty($security["icon"])) : ?>
+                                <div class="security-icon">
+                                    <img src="<?php echo $security["icon"]["url"] ?>"
+                                        alt="<?php echo @$security["icon"]["alt"] ?>">
+                                </div>
+                            <?php endif; ?>
+                            <div class="security-content">
+                                <h4><?php echo $security["title"] ?></h4>
+                                <p><?php echo $security["text"] ?></p>
                             </div>
-                        <?php endif; ?>
-                        <div class="security-content">
-                            <h4><?php echo $security["title"] ?></h4>
-                            <p><?php echo $security["text"] ?></p>
                         </div>
-                    </div>
-                <?php endforeach; endif; ?>
+                <?php endforeach;
+                endif; ?>
             </div>
         </div>
     </section>
