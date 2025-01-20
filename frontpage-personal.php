ce2ab9b5c6f86e31253fe9136e588c61471ea12f
Shreya Shah <shreya@darwindigital.com>
2025-01-20 18:23:04 +0530

    Updated 2 sections on personal landing page as per new figma design

--- frontpage-personal.php
+++ frontpage-personal.php
@@ -11,45 +11,6 @@ get_header(); ?>
 <?php include "template-parts/personal-landing/hero-section.php" ?>
 <!-- End: Hero -->
 
-<!-- Start: Payments -->
-<?php include "template-parts/personal-landing/banking-slider.php" ?>
-<!-- End: Payments -->
-
-<!-- Start: Multi Currency -->
-<?php include "template-parts/personal-landing/multi-currency-cards.php" ?>
-<!-- End: Multi Currency -->
-
-<!-- Start: Monitor Transactions -->
-<?php include "template-parts/personal-landing/account-management.php" ?>
-<!-- End: Monitor Transactions -->
-
-<!-- Start: Payment Methods -->
-<?php include "template-parts/personal-landing/corporate-cards.php" ?>
-<!-- End: Payment Methods -->
-
-<!-- Start: Payments -->
-<?php include "template-parts/personal-landing/services-section.php" ?>
-<!-- End: Payments -->
-
-<!-- Start: Money in Account -->
-<?php include "template-parts/personal-landing/solution-section.php" ?>
-<!-- End: Money in Account -->
-
-<!-- Start: Mobile App -->
-<?php include "template-parts/personal-landing/mobile-app-section.php" ?>
-<!-- End: Mobile App -->
-
-<!-- Start: Trusted Account -->
-<?php include "template-parts/personal-landing/trusted-account.php" ?>
-<!-- Start: Trusted Account -->
-
-<!-- Start: Pricing -->
-<?php include "template-parts/personal-landing/pricing-plans.php" ?>
-<!-- End: Pricing -->
-
-<!-- Start: Biometric -->
-<?php include "template-parts/personal-landing/biometric.php" ?>
-<!-- Start: Biometric -->
 
 <!-- Start: Security -->
 <?php include "template-parts/home/security.php" ?>
--- template-parts/home/cta-section.php
+++ template-parts/home/cta-section.php
@@ -1,36 +1,34 @@
 <?php
 $cta_section = get_field("cta_section");
 if (isset($cta_section)):
-    ?>
+?>
     <section class="cta">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xxl-10">
+            <div class="row justify-content-center text-center">
+                <div class="col-xxl-8">
                     <div class="cta--content-wrapper">
                         <div class="cta--content">
-                            <div class="row">
-                                <div class="col-lg-7 col-xl-7">
-                                    <h3 class="section-title section-title--lg"><?php echo $cta_section["title"] ?></h3>
+                            <h3 class="section-title section-title--xl"><?php echo $cta_section["title"] ?></h3>
 
-                                    <div class="btn-grp">
-                                        <?php if (!empty($cta_section["button_text_1"])): ?>
-                                            <a href="<?php echo @$cta_section["button_link_1"] ?>"
-                                               title="<?php echo $cta_section["button_text_1"] ?>"
-                                               class="btn btn-light-green"><?php echo $cta_section["button_text_1"] ?></a>
-                                        <?php endif; ?>
-                                        <?php if (!empty($cta_section["button_text_2"])): ?>
-                                            <a href="<?php echo $cta_section["button_link_2"] ?>"
-                                               title="<?php echo $cta_section["button_text_2"] ?>"
-                                               class="btn btn-outline-light-green"><?php echo $cta_section["button_text_2"] ?></a>
-                                        <?php endif; ?>
-                                    </div>
-                                </div>
+                            <p>Simplify your global transactions, and manage your personal finances conveniently — all in one place with PaayCo.</p>
+
+                            <div class="btn-grp justify-content-center">
+                                <?php if (!empty($cta_section["button_text_1"])): ?>
+                                    <a href="<?php echo @$cta_section["button_link_1"] ?>"
+                                        title="<?php echo $cta_section["button_text_1"] ?>"
+                                        class="btn btn-light-green"><?php echo $cta_section["button_text_1"] ?></a>
+                                <?php endif; ?>
+                                <?php if (!empty($cta_section["button_text_2"])): ?>
+                                    <a href="<?php echo $cta_section["button_link_2"] ?>"
+                                        title="<?php echo $cta_section["button_text_2"] ?>"
+                                        class="btn btn-white"><?php echo $cta_section["button_text_2"] ?></a>
+                                <?php endif; ?>
                             </div>
                         </div>
                         <div class="cta--img">
                             <?php if (!empty($cta_section["image"])): ?>
                                 <img src="<?php echo $cta_section["image"]["url"]; ?>"
-                                     alt="<?php echo @$cta_section["image"]["alt"]; ?>">
+                                    alt="<?php echo @$cta_section["image"]["alt"]; ?>">
                             <?php endif; ?>
                         </div>
                     </div>
