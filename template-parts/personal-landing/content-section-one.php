4bc84f9ccfff92f49583ca221119774dba32ba36
Shreya Shah <shreya@darwindigital.com>
2025-01-21 18:16:55 +0530

    UI updates

---
+++ template-parts/personal-landing/content-section-one.php
@@ -0,0 +1,18 @@
+<section class="personal--content-section-one">
+  <div class="container">
+    <div class="row align-items-center justify-content-between">
+      <div class="col-xl-6">
+        <div class="title-row">
+          <h3 class="section-title section-title--lg"><span>Unbeatable</span> transfer rates that save you more</h3>
+          <p>Enjoy seamless transactions with fees so low, you'll wonder why you ever paid more. No hidden fees—just clear, transparent pricing every time.</p>
+        </div>
+      </div>
+
+      <div class="col-xl-5">
+        <div class="image">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/personal/one.png" alt="Image">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
