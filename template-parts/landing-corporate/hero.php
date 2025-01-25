6f8298c82620bc7ee64726959a843cc0589a14a8
Shreya Shah <shreya@darwindigital.com>
2025-01-25 12:11:15 +0530

    Corporate landing - Hero

---
+++ template-parts/landing-corporate/hero.php
@@ -0,0 +1,22 @@
+<section class="hero hero--corporate">
+  <div class="hero--img">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/hero.png" alt="Image">
+  </div>
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="hero--content">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-xxl-9 text-center">
+            <h1>Where the world does finance</h1>
+            <p>Financial management made easy in a single platform</p>
+
+            <div class="btn-grp justify-content-center mt-5">
+              <a href="#" title="Start with payments" class="btn btn-bright-green">Start with payments</a>
+              <a href="#" title="Open free account" class="btn btn-white">Open free account</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
