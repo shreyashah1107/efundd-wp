3024cddd88b6790911f6d2956ac774a78fc6a800
Shreya Shah <shreya@darwindigital.com>
2025-02-09 15:29:44 +0530

    Landing page updates

--- template-parts/landing-corporate/content-section-nine.php
+++ template-parts/landing-corporate/content-section-nine.php
@@ -1,6 +1,74 @@
 <section class="corporate-content-section--nine">
-  <div class="content-wrapper d-flex align-items-center">
-    <div class="coin-swipe d-none d-md-block">
+  <div class="content-wrapper">
+    <div>
+      <div class="fixed-text fixed-text--top">
+        <h3>28+ Payment Methods</h3>
+      </div>
+
+      <div>
+        <div class="flags-row flags-row--one">
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-1.png" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-2.png" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-3.png" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-4.png" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-5.png" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-4.png" alt="Image">
+          </div>
+        </div>
+
+        <div class="flags-row flags-row--two">
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-2.svg" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-3.svg" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-4.svg" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-5.svg" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-6.svg" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-7.svg" alt="Image">
+          </div>
+
+          <div class="image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-8.svg" alt="Image">
+          </div>
+        </div>
+      </div>
+
+      <div class="fixed-text fixed-text--bottom">
+        <h3>40+ Currencies</h3>
+      </div>
+    </div>
+
+    <!-- <div class="coin-swipe d-none d-md-block">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/coin-swipe.png" alt="Image">
     </div>
 
@@ -34,6 +102,6 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/coin-9.png" alt="Image">
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </section>
