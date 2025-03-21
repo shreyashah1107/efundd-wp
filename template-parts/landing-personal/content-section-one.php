63d2a8387b324ca9e8b3f214470175346311d938
Shreya Shah <shreya@darwindigital.com>
2025-03-21 17:20:24 +0530

    Landing pages' updates

--- template-parts/landing-personal/content-section-one.php
+++ template-parts/landing-personal/content-section-one.php
@@ -8,8 +8,92 @@
         <div class="col-xxl-11">
           <div class="row align-items-center">
             <div class="col-lg-5 col-xl-6 order-last order-lg-first mb-5 mb-lg-0">
-              <div class="image text-center">
+              <!-- <div class="image text-center">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/personal/one.png" alt="Image">
+              </div> -->
+
+              <div class="currency-conversion-form--wrapper">
+                <div class="currency-conversion-form">
+                  <h4>How much are you sending?</h4>
+
+                  <form action="">
+                    <div class="form-group">
+                      <div class="input-group">
+                        <div class="input-group-text input-group-text--prepend">
+                          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-2.svg" alt="USD">
+                          <div>You are sending to</div>
+                        </div>
+
+                        <select class="form-select">
+                          <option selected disabled>Choose Country</option>
+                          <option value="US">United States</option>
+                          <option value="UK">United Kingdom</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label>You send</label>
+                      <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Enter amount" aria-label="Enter amount">
+
+                        <span class="input-group-text">
+                          <div class="currency-selector">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-1.svg" alt="GBP">
+                            <select class="form-select">
+                              <option value="GBP">GBP</option>
+                              <option value="USD">USD</option>
+                            </select>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label>Recipient gets</label>
+                      <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Enter amount" aria-label="Enter amount">
+
+                        <span class="input-group-text">
+                          <div class="currency-selector">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/country-2.svg" alt="USD">
+                            <select class="form-select">
+                              <option value="USD">USD</option>
+                              <option value="GBP">GBP</option>
+                            </select>
+                          </div>
+                        </span>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label>Pay from</label>
+                      <div class="input-group">
+                        <div class="input-group-text input-group-text--prepend">
+                          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/icon-select-pay.png" alt="">
+                        </div>
+
+                        <select class="form-select">
+                          <option value="GBP">PaayCo GBP balance</option>
+                          <option value="UK">United Kingdom</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div class="fees-summary">
+                      <div>
+                        <label>Our fees</label>
+                        <p>0.04 GBP</p>
+                      </div>
+                      <div>
+                        <label>Total</label>
+                        <p>0.04 GBP</p>
+                      </div>
+                    </div>
+
+                    <a href="#" class="btn btn-bright-green btn-block">Send money</a>
+                  </form>
+                </div>
               </div>
             </div>
             <div class="col-lg-7 col-xl-6 text-lg-start">
