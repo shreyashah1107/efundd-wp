c1a77e3eff81ffa7b68b9309ce4cf7c6c6cf09a8
Shreya Shah <shreya@darwindigital.com>
2025-03-21 16:08:03 +0530

    Landing pages' updates

--- template-parts/landing-business/content-section-eleven.php
+++ template-parts/landing-business/content-section-eleven.php
@@ -17,7 +17,7 @@
               </div>
               <div class="text">
                 <h4>PCI DSS, SOC 2 and ISO 27001 certified</h4>
-                <p>We securely store your money with reputable financial institutions, keeping it separate from our own accounts, and it remains inaccessible to our partners during the normal course of business.</p>
+                <p>Our organization has achieved compliance with both the Payment Card Industry Data Security Standard (PCI DSS) - specifically focused on protecting credit card data - and the International Organization for Standardization (ISO) 27001 standard.</p>
               </div>
             </div>
 
@@ -36,8 +36,8 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/business/bank.svg" alt="Bank">
               </div>
               <div class="text">
-                <h4>Protected by Top Financial Institutions</h4>
-                <p>We securely store your money with reputable financial institutions, keeping it separate from our own accounts, and it remains inaccessible to our partners during the normal course of business.</p>
+                <h4>FINANCIAL SERVICES COMPENSATION SCHEME</h4>
+                <p>The Financial Services Compensation Scheme (FSCS) protects up to £85,000 per person, per financial institution.</p>
               </div>
             </div>
 
@@ -46,8 +46,8 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/business/bank.svg" alt="Bank">
               </div>
               <div class="text">
-                <h4>Protected by Top Financial Institutions</h4>
-                <p>We securely store your money with reputable financial institutions, keeping it separate from our own accounts, and it remains inaccessible to our partners during the normal course of business.</p>
+                <h4>PURPOSE OF SAFEGUARDED ACCOUNT</h4>
+                <p>Safeguarding is used by Payment Institutions and E-Money Issuers to protect funds it holds in-trust for the beneficial use of underlying users in the event they were to go into liquidation. It ensures that the funds can only be used to complete the payment as instructed or, should that prove to be impossible, the funds are returned to the original payer.</p>
               </div>
             </div>
           </div>
