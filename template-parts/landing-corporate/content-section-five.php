31db8fec52fdf163e961a2618a3e6ff5750887e8
Shreya Shah <shreya@darwindigital.com>
2025-04-07 11:40:33 +0530

    Corporate Hero & content section five changes

--- template-parts/landing-corporate/content-section-five.php
+++ template-parts/landing-corporate/content-section-five.php
@@ -5,7 +5,7 @@ if ($fourth_content_section) {
     $primary_button = $fourth_content_section["primary_button"];
     $secondary_button = $fourth_content_section["secondary_button"];
     $cards = $fourth_content_section["cards"];
-    ?>
+?>
     <section class="corporate-content-section--five">
         <div class="content-wrapper">
             <div class="container">
@@ -17,34 +17,78 @@ if ($fourth_content_section) {
                             <div class="btn-grp justify-content-md-start flex-shrink-0">
                                 <?php if ($primary_button) { ?>
                                     <a href="<?php echo $primary_button["url"] ?>"
-                                       title="<?php echo $primary_button["title"] ?>"
-                                       class="btn btn-bright-green wow fadeInUp"
-                                       data-wow-delay="0.2s">
+                                        title="<?php echo $primary_button["title"] ?>"
+                                        class="btn btn-bright-green wow fadeInUp"
+                                        data-wow-delay="0.2s">
                                         <?php echo $primary_button["title"] ?>
                                     </a>
                                 <?php } ?>
                                 <?php if ($secondary_button) { ?>
                                     <a href="<?php echo $secondary_button["url"] ?>"
-                                       title="<?php echo $secondary_button["title"] ?>"
-                                       class="btn btn-white wow fadeInUp"
-                                       data-wow-delay="0.4s"><?php echo $secondary_button["title"] ?></a>
+                                        title="<?php echo $secondary_button["title"] ?>"
+                                        class="btn btn-white wow fadeInUp"
+                                        data-wow-delay="0.4s"><?php echo $secondary_button["title"] ?></a>
                                 <?php } ?>
                             </div>
                         </div>
+
                         <?php if ($cards) { ?>
+                            <div class="sell--swiper swiper mt-5 pt-5 mt-md-0">
+                                <div class="swiper-wrapper">
+                                    <?php
+                                    foreach ($cards as $card) {
+                                        $title = $card["title"];
+                                        $text = $card["text"];
+                                        $button = $card["button"];
+                                        $image = $card["image"];
+                                    ?>
+                                        <div class="swiper-slide">
+                                            <div class="card-sell wow fadeInUp" data-wow-delay="0.2s">
+                                                <div class="card-sell--img">
+                                                    <?php if ($image) { ?>
+                                                        <img src="<?php echo $image["url"] ?>"
+                                                            alt="<?php echo @$image["alt"] ?>">
+                                                    <?php } else { ?>
+                                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/sell1.png"
+                                                            alt="<?php echo get_the_title() ?>">
+                                                    <?php } ?>
+                                                </div>
+                                                <div class="card-sell--content">
+                                                    <h4 class="title"><?php echo $title ?></h4>
+                                                    <div class="text"><?php echo $text ?>
+                                                    </div>
+                                                    <?php if (!empty($button)) { ?>
+                                                        <div class="card-sell--cta">
+                                                            <a href="<?php echo $button["url"] ?>"
+                                                                title="<?php echo $button["title"] ?>"
+                                                                class="btn btn-outline-content-link"><?php echo $button["title"] ?></a>
+                                                        </div>
+                                                    <?php } ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php
+                                    } ?>
+                                </div>
+
+                                <div class="swiper-pagination"></div>
+                            </div>
+                        <?php } ?>
+
+                        <!-- <?php if ($cards) { ?>
                             <div class="row align-items-end mt-5 pt-5 mt-md-0">
                                 <?php $l = 1;
-                                foreach ($cards as $card) {
-                                    $title = $card["title"];
-                                    $text = $card["text"];
-                                    $button = $card["button"];
-                                    $image = $card["image"];
-                                    if ($l == 1) {
-                                        $class = "col-lg-4";
-                                    } else {
-                                        $class = "col-lg-4 mt-5 mt-lg-0";
-                                    }
-                                    ?>
+                                    foreach ($cards as $card) {
+                                        $title = $card["title"];
+                                        $text = $card["text"];
+                                        $button = $card["button"];
+                                        $image = $card["image"];
+                                        if ($l == 1) {
+                                            $class = "col-lg-4";
+                                        } else {
+                                            $class = "col-lg-4 mt-5 mt-lg-0";
+                                        }
+                                ?>
                                     <div class="<?php echo $class ?>">
                                         <div class="card-sell wow fadeInUp" data-wow-delay="0.2s">
                                             <?php if ($l == 2) { ?>
@@ -55,28 +99,28 @@ if ($fourth_content_section) {
                                                     <?php if (!empty($button)) { ?>
                                                         <div class="card-sell--cta">
                                                             <a href="<?php echo $button["url"] ?>"
-                                                               title="<?php echo $button["title"] ?>"
-                                                               class="btn btn-outline-white"><?php echo $button["title"] ?></a>
+                                                                title="<?php echo $button["title"] ?>"
+                                                                class="btn btn-outline-white"><?php echo $button["title"] ?></a>
                                                         </div>
                                                     <?php } ?>
                                                 </div>
                                                 <div class="card-sell--img">
                                                     <?php if ($image) { ?>
                                                         <img src="<?php echo $image["url"] ?>"
-                                                             alt="<?php echo @$image["alt"] ?>">
+                                                            alt="<?php echo @$image["alt"] ?>">
                                                     <?php } else { ?>
                                                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/sell1.png"
-                                                             alt="<?php echo get_the_title() ?>">
+                                                            alt="<?php echo get_the_title() ?>">
                                                     <?php } ?>
                                                 </div>
                                             <?php } else { ?>
                                                 <div class="card-sell--img">
                                                     <?php if ($image) { ?>
                                                         <img src="<?php echo $image["url"] ?>"
-                                                             alt="<?php echo @$image["alt"] ?>">
+                                                            alt="<?php echo @$image["alt"] ?>">
                                                     <?php } else { ?>
                                                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/sell1.png"
-                                                             alt="<?php echo get_the_title() ?>">
+                                                            alt="<?php echo get_the_title() ?>">
                                                     <?php } ?>
                                                 </div>
                                                 <div class="card-sell--content">
@@ -86,8 +130,8 @@ if ($fourth_content_section) {
                                                     <?php if (!empty($button)) { ?>
                                                         <div class="card-sell--cta">
                                                             <a href="<?php echo $button["url"] ?>"
-                                                               title="<?php echo $button["title"] ?>"
-                                                               class="btn btn-outline-white"><?php echo $button["title"] ?></a>
+                                                                title="<?php echo $button["title"] ?>"
+                                                                class="btn btn-outline-white"><?php echo $button["title"] ?></a>
                                                         </div>
                                                     <?php } ?>
                                                 </div>
@@ -95,10 +139,10 @@ if ($fourth_content_section) {
 
                                         </div>
                                     </div>
-                                    <?php $l++;
-                                } ?>
+                                <?php $l++;
+                                    } ?>
                             </div>
-                        <?php } ?>
+                        <?php } ?> -->
                     </div>
                 </div>
             </div>
