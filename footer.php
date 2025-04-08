62cb1a5126d8860bce558aad4515a065816529ce
Shreya Shah <shreya@darwindigital.com>
2025-04-08 16:57:25 +0530

    Updated footer

--- footer.php
+++ footer.php
@@ -34,6 +34,17 @@ $main_footer_menu = render_menu('footer');
                                     ));
                                     ?>
                                 </div>
+
+                                <div class="footer--nav-col">
+                                    <h4><?php _e('Partners', 'fundd'); ?></h4>
+                                    <?php
+                                    wp_nav_menu(array(
+                                        'theme_location' => 'footer_partners',
+                                        'container' => false,
+                                        'menu_class' => 'footer--nav-list',
+                                    ));
+                                    ?>
+                                </div>
                             </div>
 
                             <div>
@@ -73,10 +84,21 @@ $main_footer_menu = render_menu('footer');
                                 </div>
 
                                 <div class="footer--nav-col">
-                                    <h4><?php _e('Company', 'fundd'); ?></h4>
+                                    <h4><?php _e('Help', 'fundd'); ?></h4>
                                     <?php
                                     wp_nav_menu(array(
-                                        'theme_location' => 'footer_company',
+                                        'theme_location' => 'footer_help',
+                                        'container' => false,
+                                        'menu_class' => 'footer--nav-list',
+                                    ));
+                                    ?>
+                                </div>
+
+                                <div class="footer--nav-col">
+                                    <h4><?php _e('Legal', 'fundd'); ?></h4>
+                                    <?php
+                                    wp_nav_menu(array(
+                                        'theme_location' => 'footer_legal',
                                         'container' => false,
                                         'menu_class' => 'footer--nav-list',
                                     ));
@@ -85,40 +107,41 @@ $main_footer_menu = render_menu('footer');
                             </div>
 
                             <div>
-                                <div class="footer--nav-col">
-                                    <div class="footer--company">
-                                        <div class="footer--company-top">
-                                            <?php if ($col_1):
-                                                $support_email = $col_1["support_email"];
-                                                $phone = $col_1["phone"];
-                                                $developers_email = $col_1["developers_email"];
-                                                $sales_email = $col_1["sales_email"];
-                                            ?>
+                                <?php if ($col_1):
+                                    $support_email = $col_1["support_email"];
+                                    $phone = $col_1["phone"];
+                                    $developers_email = $col_1["developers_email"];
+                                    $sales_email = $col_1["sales_email"];
+                                ?>
+                                    <div class="footer--nav-col">
+                                        <div class="footer--company">
+                                            <div class="footer--company-top">
+
                                                 <div class="contact-info">
                                                     <?php if ($support_email || $phone): ?>
                                                         <div class="contact-info--col">
-                                                            <h5><?php _e("Support", "darwin") ?></h5>
-                                                            <div><a href="tel:<?php echo $phone ?>"
-                                                                    title="<?php echo $phone ?>"><?php echo $phone ?></a>
-                                                            </div>
+                                                            <h4><?php _e("Support", "darwin") ?></h4>
                                                             <div><a href="mailto:<?php echo $support_email ?>"
                                                                     title="<?php echo $support_email ?>"><?php echo $support_email ?></a>
+                                                            </div>
+                                                            <div><a href="tel:<?php echo $phone ?>"
+                                                                    title="<?php echo $phone ?>"><?php echo $phone ?></a>
                                                             </div>
                                                         </div>
                                                     <?php endif; ?>
                                                     <?php if ($developers_email): ?>
                                                         <div class="contact-info--col">
-                                                            <h5><?php _e("Developers", "darwin") ?></h5>
+                                                            <h4><?php _e("Developers", "darwin") ?></h4>
                                                             <div><a href="mailto:<?php echo $developers_email ?>"
                                                                     title="<?php echo $developers_email ?>"><?php echo $developers_email ?></a>
                                                             </div>
                                                         </div>
                                                     <?php endif; ?>
                                                 </div>
-                                            <?php endif; ?>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                <?php endif; ?>
 
                                 <div class="footer--nav-col">
                                     <h4><?php _e('Help Centre', 'fundd'); ?></h4>
@@ -130,12 +153,39 @@ $main_footer_menu = render_menu('footer');
                                     ));
                                     ?>
                                 </div>
+
+                                <div class="footer--nav-col">
+                                    <div class="download-list--wrapper">
+                                        <?php if ($applications_section["title"]): ?>
+                                            <h4><?php echo $applications_section["title"] ?></h4>
+                                        <?php else: ?>
+                                            <h4><?php _e("Download the Paayco app", "darwin") ?></h4>
+                                        <?php endif; ?>
+                                        <div class="download-list">
+                                            <div>
+                                                <a href="<?php echo $applications_section["ios_app_url"] ?>"
+                                                    title="<?php _e("Download on App Store", "darwin") ?>" target="_blank">
+                                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo/app-store.png"
+                                                        alt="<?php _e("Download on App Store", "darwin") ?>">
+                                                </a>
+                                            </div>
+
+                                            <div>
+                                                <a href="<?php echo $applications_section["android_app_url"] ?>"
+                                                    title="<?php _e("Download on Google Play", "darwin") ?>" target="_blank">
+                                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo/google-pay.png"
+                                                        alt="<?php _e("Download on Google Play", "darwin") ?>">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <!-- Quick Links -->
-                    <div class="footer--quick-links">
+                    <div class="footer--quick-links" style="display: none;">
                         <div class="footer--nav-grid">
                             <?php
                             // Fetch the menu items
