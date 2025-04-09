b0d49ab23a2f2bbe2ebae95c4b59a77b9a1bafb6
Shreya Shah <shreya@darwindigital.com>
2025-04-09 16:41:54 +0530

    Feedback

--- template-parts/landing-corporate/hero.php
+++ template-parts/landing-corporate/hero.php
@@ -8,15 +8,15 @@ if ($hero_section) {
     $banner_image = $hero_section['banner_image'];
     $certification_text = $hero_section['certification_text'];
     $certificates = $hero_section['certificates'];
-    ?>
+?>
     <section class="hero hero--corporate">
         <div class="hero--img">
             <?php if ($banner_image) { ?>
                 <img src="<?php echo $banner_image["url"] ?>"
-                     alt="<?php echo @$banner_image["alt"] ?>">
+                    alt="<?php echo @$banner_image["alt"] ?>">
             <?php } else { ?>
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/hero.jpg"
-                     alt="<?php echo get_the_title() ?>">
+                    alt="<?php echo get_the_title() ?>">
             <?php } ?>
 
         </div>
@@ -31,17 +31,17 @@ if ($hero_section) {
                             <div class="btn-grp justify-content-center mt-5">
                                 <?php if ($primary_button) { ?>
                                     <a href="<?php echo $primary_button["url"] ?>"
-                                       title="<?php echo $primary_button["title"] ?>"
-                                       class="btn btn-bright-green wow fadeInUp"
-                                       data-wow-delay="0.2s">
+                                        title="<?php echo $primary_button["title"] ?>"
+                                        class="btn btn-bright-green wow fadeInUp"
+                                        data-wow-delay="0.2s">
                                         <?php echo $primary_button["title"] ?>
                                     </a>
                                 <?php } ?>
                                 <?php if ($secondary_button) { ?>
                                     <a href="<?php echo $secondary_button["url"] ?>"
-                                       title="<?php echo $secondary_button["title"] ?>"
-                                       class="btn btn-white wow fadeInUp"
-                                       data-wow-delay="0.4s"><?php echo $secondary_button["title"] ?></a>
+                                        title="<?php echo $secondary_button["title"] ?>"
+                                        class="btn btn-white wow fadeInUp"
+                                        data-wow-delay="0.4s"><?php echo $secondary_button["title"] ?></a>
                                 <?php } ?>
                             </div>
                         </div>
@@ -49,7 +49,7 @@ if ($hero_section) {
                 </div>
             </div>
 
-            <div class="certificates-row--content">
+            <div class="certificates-row--content" style="display: none;">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-xxl-11">
@@ -67,20 +67,20 @@ if ($hero_section) {
                                                 if ($i == 0) { ?>
                                                     <div class="swiper-slide wow fadeInUp">
                                                         <img src="<?php echo $certificate["url"] ?>"
-                                                             alt="<?php echo @$certificate["alt"] ?>">
+                                                            alt="<?php echo @$certificate["alt"] ?>">
                                                     </div>
                                                 <?php } else {
 
                                                     $delay = $delay + 0.2;
-                                                    ?>
+                                                ?>
                                                     <div class="swiper-slide wow fadeInUp"
-                                                         data-wow-delay="<?php echo $delay ?>s">
+                                                        data-wow-delay="<?php echo $delay ?>s">
                                                         <img src="<?php echo $certificate["url"] ?>"
-                                                             alt="<?php echo @$certificate["alt"] ?>">
+                                                            alt="<?php echo @$certificate["alt"] ?>">
                                                     </div>
                                                 <?php } ?>
 
-                                                <?php $i++;
+                                            <?php $i++;
                                             } ?>
                                         </div>
                                     </div>
