82c4306e848949c59332a814353d649ebc3f06bf
Shreya Shah <shreya@darwindigital.com>
2025-04-17 18:25:43 +0530

    Feedback fixes - landing pages

--- template-parts/landing-personal/content-section-two.php
+++ template-parts/landing-personal/content-section-two.php
@@ -5,7 +5,7 @@ if ($first_content_section) {
     $text = $first_content_section['text'];
     $button = $first_content_section['button'];
     $image = $first_content_section['image'];
-    ?>
+?>
     <section class="personal-content-section--two">
         <div class="content-wrapper">
             <div class="container">
@@ -14,7 +14,7 @@ if ($first_content_section) {
                         <div class="row align-items-center justify-content-center justify-content-lg-start">
                             <div class="col-lg-7 col-xl-7 mb-5 mb-lg-0">
                                 <div class="title-row mb-4">
-                                    <h3 class="section-title section-title--xl text-anime-style-3"><?php echo $title ?></h3>
+                                    <h3 class="section-title section-title--xl wow fadeInUp" data-wow-delay="0.1s"><?php echo $title ?></h3>
                                 </div>
 
                                 <div class="option-list--items">
@@ -24,7 +24,7 @@ if ($first_content_section) {
                                 <div class="btn-grp">
                                     <?php if ($button) { ?>
                                         <a href="<?php echo @$button["url"] ?>" title="<?php echo $button["title"] ?>" class="btn btn-bright-green wow fadeInUp"
-                                           data-wow-delay="0.2s"><?php echo $button["title"] ?></a>
+                                            data-wow-delay="0.2s"><?php echo $button["title"] ?></a>
                                     <?php } ?>
                                 </div>
                             </div>
@@ -32,7 +32,7 @@ if ($first_content_section) {
                                 <div class="image text-center wow fadeInRight" data-wow-delay="0.5s">
                                     <?php if ($image) { ?>
                                         <img src="<?php echo $image["url"] ?>"
-                                             alt="<?php echo @$image["alt"] ?>">
+                                            alt="<?php echo @$image["alt"] ?>">
                                     <?php } ?>
                                 </div>
                             </div>
