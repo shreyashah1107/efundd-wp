69e939afa6509efa17a73ef5096cd0542d5146b8
Shreya Shah <shreya@darwindigital.com>
2025-04-18 15:57:27 +0530

    Responsive lang dropdown

--- header.php
+++ header.php
@@ -5,9 +5,7 @@
     <meta charset="<?php bloginfo('charset'); ?>" />
 
 
-    <title><?php wp_title(''); ?><?php if (wp_title('', false)) {
-                                        echo ' :';
-                                    } ?><?php bloginfo('name'); ?></title>
+    <title><?php wp_title(''); ?><?php bloginfo('name'); ?></title>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
