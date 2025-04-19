6a1e44f9d4ed7895aafd7274a273769be434272b
shreyashah1107 <shreyashah.117@gmail.com>
2025-04-19 18:00:44 +0530

    Payment Link : Hero

---
+++ template-parts/sub-payment-link/hero.php
@@ -0,0 +1,28 @@
+<section class="hero hero--payment-link">
+  <div class="section-bg section-bg--hero">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/personal/section-bg.png" alt="Image">
+  </div>
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="hero--content text-start">
+        <div class="row justify-content-center">
+          <div class="col-xxl-11">
+            <div class="row">
+              <div class="col-xl-7 col-xxl-8">
+                <h1 class="text-anime-style-3">Accept instant payments </h1>
+                <p class="wow fadeInUp" data-wow-delay="0.1s">Our Payment Links enables you to accept instant payments without a website. Set up a custom payment page with unique invoice details or accept general payments on the link.. Absolutely no code needed..</p>
+                <div class="btn-grp">
+                  <a href="#" title="Start Earning Cashback" class="btn btn-bright-green wow fadeInUp" data-wow-delay="0.2s"> Start Earning Cashback </a>
+                  <a href="#" title="Learn more" class="btn btn-outline-white wow fadeInUp" data-wow-delay="0.4s">Learn more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hero--content-images">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-link/hero.png" alt="Image">
+    </div>
+  </div>
+</section>
