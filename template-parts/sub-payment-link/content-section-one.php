34a734b7134ce3e7e15d2f08a77dcedc63945713
shreyashah1107 <shreyashah.117@gmail.com>
2025-04-19 19:06:57 +0530

    Payment Link : Content sections one/two

---
+++ template-parts/sub-payment-link/content-section-one.php
@@ -0,0 +1,39 @@
+<section class="payment-link-content-section--one">
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-11">
+          <div class="title-row text-center">
+            <h3 class="section-title section-title--xl text-anime-style-3">How we've simplify payments</h3>
+          </div>
+
+          <div class="feature-cards pt-5 pt-lg-0 pt-xl-5">
+            <div class="feature-card wow fadeInUp" data-wow-delay="0.1s">
+              <div class="feature-card__icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-link/icon-link.svg" alt="Image">
+              </div>
+              <h4 class="feature-card__title">Instant payment link</h4>
+              <p class="feature-card__text">Create Payment Links in just a few clicks – directly from your Dashboard, with absolutely no code.</p>
+            </div>
+
+            <div class="feature-card wow fadeInUp" data-wow-delay="0.2s">
+              <div class="feature-card__icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-link/icon-card.svg" alt="Image">
+              </div>
+              <h4 class="feature-card__title">Simple payment method</h4>
+              <p class="feature-card__text">Payment links are the simplest way to accept payments made with Visa, Mastercard or Maestro cards.</p>
+            </div>
+
+            <div class="feature-card wow fadeInUp" data-wow-delay="0.3s">
+              <div class="feature-card__icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-link/icon-bolt.svg" alt="Image">
+              </div>
+              <h4 class="feature-card__title">Faster Payouts</h4>
+              <p class="feature-card__text">Payments are processed quickly, ensuring that businesses receive their funds faster without unnecessary delays..</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ template-parts/sub-payment-link/content-section-two.php
@@ -0,0 +1,26 @@
+<section class="payment-link-content-section--two">
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-11">
+          <div class="row align-items-center">
+            <div class="col-lg-5 col-xl-6 order-last order-lg-first">
+              <div class="image text-center wow fadeInLeft" data-wow-delay="0.2s">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/payment-link/two.png" alt="Image">
+              </div>
+            </div>
+            <div class="col-lg-7 col-xl-6 mb-5 mb-lg-0">
+              <div class="title-row mb-4 mb-lg-5">
+                <h3 class="section-title section-title--xl text-anime-style-3"> A no-code way to get paid </h3>
+              </div>
+              <p class="wow fadeInUp" data-wow-delay="0.1s">Easy, safe and free. Simply share a link with your customers for them to pay securely in a matter of minutes</p>
+              <div class="btn-grp mt-5">
+                <a href="#" title="Create yours now" class="btn btn-bright-green wow fadeInUp" data-wow-delay="0.2s"> Create yours now </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
