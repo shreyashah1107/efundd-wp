5c40d8dd58b388992345bd431c1e382959bffec7
shreyashah1107 <shreyashah.117@gmail.com>
2025-04-19 17:25:15 +0530

    Blank files & template created for payment link ui

---
+++ subpage-payment-link.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Template Name: Payment Link Template
+ */
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+get_header(); ?>
+    <!-- Start: Hero -->
+    <?php include "template-parts/sub-payment-link/hero.php" ?>
+    <!-- End: Hero -->
+
+    <!-- Start: Content Section One -->
+    <?php include "template-parts/sub-payment-link/content-section-one.php" ?>
+    <!-- End: Content Section One -->
+
+    <!-- Start: Content Section Two -->
+    <?php include "template-parts/sub-payment-link/content-section-two.php" ?>
+    <!-- End: Content Section Two -->
+
+    <!-- Start: Content Section Three -->
+    <?php include "template-parts/sub-payment-link/content-section-three.php" ?>
+    <!-- End: Content Section Three -->
+
+    <!-- Start: Content Section Four -->
+    <?php include "template-parts/sub-payment-link/content-section-four.php" ?>
+    <!-- End: Content Section Four -->
+
+    <!-- Start: Content Section Five -->
+    <?php include "template-parts/sub-payment-link/content-section-five.php" ?>
+    <!-- End: Content Section Five -->
+
+    <!-- Start: Content Section Six -->
+    <?php include "template-parts/sub-payment-link/content-section-six.php" ?>
+    <!-- End: Content Section Six -->
+
+    <!-- Start: Content Section Seven -->
+    <?php include "template-parts/sub-payment-link/content-section-seven.php" ?>
+    <!-- End: Content Section Seven -->
+
+    <!-- Start: Content Section Eight -->
+    <?php include "template-parts/sub-payment-link/content-section-eight.php" ?>
+    <!-- End: Content Section Eight -->
+<?php get_footer(); ?>
