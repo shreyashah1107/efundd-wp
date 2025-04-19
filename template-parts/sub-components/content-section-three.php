5a79ea4063a6d31cad4798266365bc970be73b82
shreyashah1107 <shreyashah.117@gmail.com>
2025-04-19 18:41:13 +0530

    Components UI - responsive fixes

--- template-parts/sub-components/content-section-three.php
+++ template-parts/sub-components/content-section-three.php
@@ -3,13 +3,13 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-xxl-11">
-          <div class="row align-items-center justify-content-center justify-content-lg-start text-center text-lg-start">
+          <div class="row align-items-center justify-content-center justify-content-lg-start">
             <div class="col-lg-6 col-xl-6 mb-5 mb-lg-0">
               <div class="title-row mb-5">
                 <h3 class="section-title section-title--xl mb-5 text-anime-style-3">Simplify your payments with the Paayco API .</h3>
                 <p class="wow fadeInUp" data-wow-delay="0.2s">Create the best user experience by easily integrating Paayco compliant API with your website or app for a simple and powerful payment experience.</p>
               </div>
-              <div class="btn-grp justify-content-center justify-content-lg-start">
+              <div class="btn-grp mt-5">
                 <a href="#" title="Explore API docs" class="btn btn-bright-green wow fadeInUp" data-wow-delay="0.2s">Explore API docs</a>
               </div>
             </div>
--- template-parts/sub-components/content-section-two.php
+++ template-parts/sub-components/content-section-two.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-xxl-11">
-          <div class="row align-items-center justify-content-center justify-content-lg-start text-center text-lg-start">
+          <div class="row align-items-center justify-content-center justify-content-lg-start">
             <div class="col-md-8 col-lg-6 col-xl-6 order-last order-lg-first">
               <div class="image text-center wow fadeInLeft" data-wow-delay="0.2s">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/components/two.png" alt="Image">
@@ -14,7 +14,7 @@
                 <h3 class="section-title section-title--xl text-anime-style-3">Control the full user experience</h3>
               </div>
               <p class="wow fadeInUp" data-wow-delay="0.1s">Our flexible APIs allows you to use pre-built components and custom  design the UIs. Use JavaScript or React  to build your ideal web integration or our iOS, Android, or React Native SDKs to design a custom mobile flow.</p>
-              <div class="btn-grp justify-content-center justify-content-lg-start mt-5">
+              <div class="btn-grp mt-5">
                 <a href="#" title="Sign up" class="btn btn-bright-green wow fadeInUp" data-wow-delay="0.2s"> Sign up </a>
               </div>
             </div>
