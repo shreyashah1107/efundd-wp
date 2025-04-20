4787fdc888f5230618472a29000cc7c9921acd40
shreyashah1107 <shreyashah.117@gmail.com>
2025-04-20 12:25:40 +0530

    Contact UI

--- template-parts/sub-contact/cta.php
+++ template-parts/sub-contact/cta.php
@@ -4,10 +4,10 @@
       <div class="row justify-content-center">
         <div class="col-xxl-11">
           <div class="cta-content--wrapper">
-            <div class="text">
+            <div class="text wow fadeInLeft" data-wow-delay="0.1s">
               <h4>Still need help?</h4>
             </div>
-            <div class="link">
+            <div class="link wow fadeInRight" data-wow-delay="0.1s">
               <a href="#" title="Chat with an agent" class="btn btn-bright-green">Chat with an agent</a>
             </div>
           </div>
