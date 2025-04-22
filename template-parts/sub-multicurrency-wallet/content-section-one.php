33a28f2e6063f2aa25ff9d58c9a51002a05699e2
Shreya Shah <shreya@darwindigital.com>
2025-04-22 18:02:29 +0530

    Multicurrency wallet

--- template-parts/sub-multicurrency-wallet/content-section-one.php
+++ template-parts/sub-multicurrency-wallet/content-section-one.php
@@ -1,20 +1,41 @@
 <section class="multicurrency-wallet-content-section--one">
-  <div class="section-bg section-bg--right">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/section-right-bg.svg" alt="Image">
+  <div class="section-bg section-bg--center">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/redesign/corporate/section-center-bg.png" alt="Image">
   </div>
   <div class="content-wrapper">
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-xxl-8">
           <div class="title-row text-center">
-            <h3 class="section-title section-title--xl text-anime-style-3">Send & Receive Money in Multiple Currencies</h3>
+            <h4 class="section-title section-title--xl text-anime-style-3">Send & Receive Money in Multiple Currencies</h4>
             <p class="wow fadeInUp" data-wow-delay="0.1s">Paayco offers a simple solution to create recurring or one-time payments. </p>
           </div>
         </div>
       </div>
 
       <div class="row justify-content-center mt-2 mt-md-5 mt-lg-0">
-        <div class="col-xxl-11">
+        <div class="col-xxl-9">
+          <div class="currency--items">
+            <div class="currency--item">
+              <h4>Hold & Manage Multiple Currencies</h4>
+              <p>tore and organize funds in various currencies without hidden fees.</p>
+            </div>
+
+            <div class="currency--item">
+              <h4>Seamless International Payments</h4>
+              <p>Send and receive money across borders with competitive exchange rates.</p>
+            </div>
+
+            <div class="currency--item">
+              <h4>Automatic Currency Conversion</h4>
+              <p>Pay in the right currency instantly, at the best rates.</p>
+            </div>
+
+            <div class="currency--item">
+              <h4>Real-Time Balance Overview</h4>
+              <p>Get a clear, up-to-date view of all your currency balances.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
