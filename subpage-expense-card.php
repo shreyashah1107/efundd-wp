170c218c5bd0b829460da6aebf67c85ad2ed66a0
Shreya Shah <shreya@darwindigital.com>
2025-04-24 15:07:32 +0530

    Expense card template created

---
+++ subpage-expense-card.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Template Name: Expense Card Template
+ */
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+get_header(); ?>
+    <!-- Start: Hero -->
+    <?php include "template-parts/sub-expense-card/hero.php" ?>
+    <!-- End: Hero -->
+
+    <!-- Start: Content Section One -->
+    <?php include "template-parts/sub-expense-card/content-section-one.php" ?>
+    <!-- End: Content Section One -->
+
+    <!-- Start: Content Section Two -->
+    <?php include "template-parts/sub-expense-card/content-section-two.php" ?>
+    <!-- End: Content Section Two -->
+
+    <!-- Start: Content Section Three -->
+    <?php include "template-parts/sub-expense-card/content-section-three.php" ?>
+    <!-- End: Content Section Three -->
+
+    <!-- Start: Content Section Four -->
+    <?php include "template-parts/sub-expense-card/content-section-four.php" ?>
+    <!-- End: Content Section Four -->
+
+    <!-- Start: Content Section Five -->
+    <?php include "template-parts/sub-expense-card/content-section-five.php" ?>
+    <!-- End: Content Section Five -->
+
+    <!-- Start: Content Section Six -->
+    <?php include "template-parts/sub-expense-card/content-section-six.php" ?>
+    <!-- End: Content Section Six -->
+
+    <!-- Start: Content Section Seven -->
+    <?php include "template-parts/sub-expense-card/content-section-seven.php" ?>
+    <!-- End: Content Section Seven -->
+
+    <!-- Start: Content Section Eight -->
+    <?php include "template-parts/sub-expense-card/content-section-eight.php" ?>
+    <!-- End: Content Section Eight -->
+<?php get_footer(); ?>
